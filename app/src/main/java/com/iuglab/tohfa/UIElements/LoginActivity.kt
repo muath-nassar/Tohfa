70c372abc403a6bb85686a3d77d77a27a0a15ca0
MuathNassar <43493047+MuathNassar@users.noreply.github.com>
2020-04-20 22:16:58 +0300

    the basic architecture of the  project add splash and login (empty)

--- app/src/main/java/com/iuglab/tohfa/UIElements/LoginActivity.kt
+++ app/src/main/java/com/iuglab/tohfa/UIElements/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.iuglab.tohfa
+package com.iuglab.tohfa.UIElements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iuglab.tohfa.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/iuglab/tohfa/UIElements/SplashScreen.kt
@@ -0,0 +1,13 @@
+package com.iuglab.tohfa.UIElements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iuglab.tohfa.R
+
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
