4beb4f0833d3f9ae16998eb492a618e3cfe7b093
D.AbdNezar <abooodnezar2017@gmail.com>
2020-04-29 04:04:55 +0200

    Revert "add all models Admin, Customer  need implementation add Chart class (ready to use) commited by Muath"
    This reverts commit 959dd45b

---
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/Settings.kt
@@ -0,0 +1,13 @@
+package com.iuglab.tohfa.ui_elements
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iuglab.tohfa.R
+
+class Settings : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
--- app/src/main/java/com/iuglab/tohfa/ui_elements/SplashScreen.kt
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/SplashScreen.kt
@@ -2,12 +2,17 @@ package com.iuglab.tohfa.ui_elements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.iuglab.tohfa.R
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar!!.hide()
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.splash_screen)
     }
 }
