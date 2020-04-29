3a5daa35cece1ba5260d2f6a0b6a2a9f2cf2a482
D.AbdNezar <abooodnezar2017@gmail.com>
2020-04-29 23:33:06 +0200

    First Commit By Abd (Login,Splash)

--- app/src/main/java/com/iuglab/tohfa/ui_elements/LoginActivity.kt
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/LoginActivity.kt
@@ -1,9 +1,11 @@
 package com.iuglab.tohfa.ui_elements
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import com.iuglab.tohfa.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,5 +14,17 @@ class LoginActivity : AppCompatActivity() {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar!!.hide()
         setContentView(R.layout.activity_login)
+
+        tvForgetPassword.setOnClickListener {
+            startActivity(Intent(this,ForgetPassword::class.java))
+        }
+
+        tvSignUp.setOnClickListener {
+            startActivity(Intent(this,SignUp::class.java))
+        }
+
+
+
+
     }
 }
