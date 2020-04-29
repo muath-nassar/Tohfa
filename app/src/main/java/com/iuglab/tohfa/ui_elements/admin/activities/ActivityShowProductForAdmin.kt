1d516866d9657b43bfc1de6c9825e794bb56ae2d
MuathNassar <43493047+MuathNassar@users.noreply.github.com>
2020-04-30 02:34:38 +0300

    Add Admin Activities Note/layout not modified

---
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/admin/activities/ActivityShowProductForAdmin.kt
@@ -0,0 +1,13 @@
+package com.iuglab.tohfa.ui_elements.admin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iuglab.tohfa.R
+
+class ActivityShowProductForAdmin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_product_for_admin)
+    }
+}
---
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/admin/activities/AdminHome.kt
@@ -0,0 +1,13 @@
+package com.iuglab.tohfa.ui_elements.admin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iuglab.tohfa.R
+
+class AdminHome : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_home)
+    }
+}
