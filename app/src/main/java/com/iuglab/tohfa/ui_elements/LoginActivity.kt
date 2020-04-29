11a7d90454aabdbcf6f1c0c85b33acb2fbbeedfa
D.AbdNezar <abooodnezar2017@gmail.com>
2020-04-29 04:49:23 +0200

    Revert "add all models Admin, Customer  need implementation add Chart class (ready to use) commited by Muath"
    This reverts commit 959dd45b

--- app/src/main/java/com/iuglab/tohfa/ui_elements/LoginActivity.kt
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/LoginActivity.kt
@@ -2,12 +2,15 @@ package com.iuglab.tohfa.ui_elements
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import com.iuglab.tohfa.R
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_login)
     }
 }
