6717a4c825ab89e40fef4f1c5e009dafaa7066e0
osama-hasanain <osamamohmed241@gmail.com>
2020-05-02 17:16:34 +0300

    Osama Code

---
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/user/activity/CategoriesActivity.kt
@@ -0,0 +1,15 @@
+package com.iuglab.tohfa.ui_elements.user.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iuglab.tohfa.R
+
+class CategoriesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categories)
+    }
+
+
+}
---
+++ app/src/main/java/com/iuglab/tohfa/ui_elements/user/activity/DetailesActivity.kt
@@ -0,0 +1,13 @@
+package com.iuglab.tohfa.ui_elements.user.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iuglab.tohfa.R
+
+class DetailesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detailes)
+    }
+}
